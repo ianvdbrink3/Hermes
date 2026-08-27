95d30a2a7f801b1807ada83b0afee57c806e105b
ianvdbrink3 <iandepianvandenbrink@gmail.com>
2026-08-27 21:04:17 +0200

    feat: link Hermes Control to autonomy dashboard

--- src/app/brain/page.tsx
+++ src/app/brain/page.tsx
@@ -1,10 +1,12 @@
 import { HermesControlV033 } from "@/components/hermes-control-v033";
 import { SystemStatusLink } from "@/components/system-status-link";
+import { AutonomyStatusLink } from "@/components/autonomy-status-link";
 
 export default function BrainPage() {
   return (
     <>
       <HermesControlV033 />
+      <AutonomyStatusLink />
       <SystemStatusLink />
     </>
   );
