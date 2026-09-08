eeefc77d76c3349d0b6166502d75f9c84443196c
ianvdbrink3 <iandepianvandenbrink@gmail.com>
2026-09-08 22:13:36 +0200

    Show live research activity above control center

--- src/app/onderzoek/page.tsx
+++ src/app/onderzoek/page.tsx
@@ -1,5 +1,11 @@
 import { ResearchControlCenter } from "@/components/research-control-center";
+import { ResearchLiveActivity } from "@/components/research-live-activity";
 
 export default function OnderzoekPage() {
-  return <ResearchControlCenter />;
+  return (
+    <>
+      <ResearchLiveActivity />
+      <ResearchControlCenter />
+    </>
+  );
 }
