d27c2b3236d3d24105cdfc9ea8f1d2543f808db6
ianvdbrink3 <iandepianvandenbrink@gmail.com>
2026-09-22 18:26:14 +0200

    Remove duplicate research live feed

--- src/app/onderzoek/page.tsx
+++ src/app/onderzoek/page.tsx
@@ -1,11 +1,5 @@
 import { ResearchControlCenter } from "@/components/research-control-center";
-import { ResearchLiveActivity } from "@/components/research-live-activity";
 
 export default function OnderzoekPage() {
-  return (
-    <>
-      <ResearchLiveActivity />
-      <ResearchControlCenter />
-    </>
-  );
+  return <ResearchControlCenter />;
 }
